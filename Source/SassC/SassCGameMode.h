a7a26fd5ec47648255eb491fe352d903c3e52e63
Timothy Russell-Wagner <atonement100@gmail.com>
2016-05-08 11:32:27 -0400

    Init

---
+++ Source/SassC/SassCGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SassCGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SASSC_API ASassCGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
