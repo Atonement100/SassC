c4f84851c6a2f78aecf480eb31ec86c5f98f7b45
Timothy Russell-Wagner <atonement100@gmail.com>
2016-05-13 23:01:58 -0400

    Not sure how this broke, gamestate doesnt update

--- Source/SassC/sassPlayer.cpp
+++ Source/SassC/sassPlayer.cpp
@@ -12,7 +12,6 @@ AsassPlayer::AsassPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
