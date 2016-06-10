2a79b8cd9756346510eafc8c9c57ce2d9233a7eb
Timothy Russell-Wagner <atonement100@gmail.com>
2016-06-09 22:39:55 -0400

    Comment playerstate for intellisense

--- Source/SassC/sassPlayerState.h
+++ Source/SassC/sassPlayerState.h
@@ -16,30 +16,42 @@ class SASSC_API AsassPlayerState : public APlayerState
 	AsassPlayerState();
 
 public:
+	/*All controlled buildings*/
 	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		TArray<AActor*> ControlledBuildings;
+	/*Currently selected units*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		TArray<AunitBase*> SelectedUnits;
+	/*Playercolor that is set on server join*/
 	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		FLinearColor PlayerColor;
 
 protected:
+	/*Spawnable currently selected to be spawned*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "PlayerState")
 		TSubclassOf<AActor> ActiveBuilding;
+	/*Current rate of gold gain*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float DeltaGold = 1;
+	/*Current quantity of gold*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float Gold = 50;
+	/*Current rate of iron gain*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float DeltaIron = 8;
+	/*Current quantity of iron*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float Iron = 50;
+	/*Current rate of food gain*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float DeltaFood = 8;
+	/*Current quantity of food*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float Food = 50;
+	/*Maximum population based on currently owned buildings*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float PopulationLimit = 7;
+	/*Current population (number of units spawned)*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "PlayerState")
 		float CurrentPopulation = 0;
 
