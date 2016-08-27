d361b6991a213cfdf93cdee84f554488dd1a89cd
Timothy Russell-Wagner <atonement100@gmail.com>
2016-08-26 21:54:41 -0400

    Trying new system for scallywag stuff... setrelativelocation doesn't replicate :/

--- Source/SassC/scallywag.cpp
+++ Source/SassC/scallywag.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SassC.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "scallywag.h"
 
 Ascallywag::Ascallywag() {
@@ -11,11 +12,26 @@ Ascallywag::Ascallywag() {
 
 void Ascallywag::Tick(float DeltaSeconds) {
 	Super::Tick(DeltaSeconds);
-	if ((GetMovementComponent()->Velocity.Size() > 0 || IsAttacking) && GetMesh()->RelativeLocation.Z < 60) {
-		//GetMesh()->AddForce(FVector(0, 0, 100));
-		GetMesh()->AddLocalOffset(FVector(0, 0, .5));
+	AddMeshRelativeLocation(GetMovementComponent()->Velocity.Size());
+}
+
+void Ascallywag::AddMeshRelativeLocation_Implementation(float Velocity){
+	if ((ProcessingMoveToBuildingOrder || ProcessingMoveToUnitOrder || ProcessingMoveToWorldOrder || IsAttacking) && UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation()-FVector(0,0,80), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::Persistent, Hit, true)) {
+		AddActorWorldOffset(FVector(0, 0, .5));
 	}
-	else if (GetMesh()->RelativeLocation.Z > -30){
-		GetMesh()->AddLocalOffset(FVector(0, 0, -.5));
+	else if (!UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation() - FVector(0, 0, 33), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::Persistent, Hit, true)) {//if (GetMesh()->RelativeLocation.Z > -30) {
+		AddActorWorldOffset(FVector(0, 0, -.5));
 	}
+}
+
+bool Ascallywag::AddMeshRelativeLocation_Validate(float Velocity){
+	return true;
+}
+
+void Ascallywag::NetAddMeshRelativeLocation_Implementation(float Velocity) {
+
+}
+
+bool Ascallywag::NetAddMeshRelativeLocation_Validate(float Velocity) {
+	return true;
 }
--- Source/SassC/scallywag.h
+++ Source/SassC/scallywag.h
@@ -21,5 +21,16 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Unit Base")
 		float ScallywagAttackRange = 70.0f;
 	
+	UFUNCTION(Reliable, Server, WithValidation)
+	void AddMeshRelativeLocation(float Velocity);
+	virtual void AddMeshRelativeLocation_Implementation(float Velocity);
+	virtual bool AddMeshRelativeLocation_Validate(float Velocity);
+
+	UFUNCTION(Reliable, NetMulticast, WithValidation)
+	void NetAddMeshRelativeLocation(float Velocity);
+	virtual void NetAddMeshRelativeLocation_Implementation(float Velocity);
+	virtual bool NetAddMeshRelativeLocation_Validate(float Velocity);
 	
+	const TArray<AActor*> ActorsToIgnore = TArray<AActor*>();
+	FHitResult Hit;
 };
