8adc9a283b9480b26ec78cce6f5a60ea340951ad
Timothy Russell-Wagner <atonement100@gmail.com>
2016-08-27 23:31:26 -0400

    Change flight debug to oneframe from persistent

--- Source/SassC/scallywag.cpp
+++ Source/SassC/scallywag.cpp
@@ -40,10 +40,10 @@ bool Ascallywag::ColorUnit_Validate(FLinearColor PlayerColor) {
 
 
 void Ascallywag::AddMeshRelativeLocation_Implementation(float Velocity){
-	if ((ProcessingMoveToBuildingOrder || ProcessingMoveToUnitOrder || ProcessingMoveToWorldOrder || IsAttacking) && UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation()-FVector(0,0,90), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::Persistent, Hit, true)) {
+	if ((ProcessingMoveToBuildingOrder || ProcessingMoveToUnitOrder || ProcessingMoveToWorldOrder || IsAttacking) && UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation()-FVector(0,0,90), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::ForOneFrame, Hit, true)) {
 		AddActorWorldOffset(FVector(0, 0, .5));
 	}
-	else if (!UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation() - FVector(0, 0, 33), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::Persistent, Hit, true)) {//if (GetMesh()->RelativeLocation.Z > -30) {
+	else if (!UKismetSystemLibrary::LineTraceSingle_NEW(GetWorld(), GetActorLocation(), GetActorLocation() - FVector(0, 0, 33), UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_GameTraceChannel4), true, ActorsToIgnore, EDrawDebugTrace::ForOneFrame, Hit, true)) {//if (GetMesh()->RelativeLocation.Z > -30) {
 		AddActorWorldOffset(FVector(0, 0, -.5));
 	}
 }
