737c872c4e76b766ab3e4db50119a87e1da4de85
Timothy Russell-Wagner <atonement100@gmail.com>
2023-02-22 23:45:34 -0500

    BuildingRequirements struct

---
+++ Source/SassC/Private/Core/BuildingRequirements.cpp
@@ -0,0 +1,1 @@
+﻿#include "Core/BuildingRequirements.h"
---
+++ Source/SassC/Public/Core/BuildingRequirements.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "BuildingType.h"
+#include "BuildingRequirements.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBuildingRequirement
+{
+	GENERATED_USTRUCT_BODY()
+
+	FBuildingRequirement(): AmountRequired(0), LevelRequired(0) {}
+
+	FBuildingRequirement(const uint8 AmountRequired, const uint8 LevelRequired):
+		AmountRequired(AmountRequired), LevelRequired(LevelRequired) {}	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 AmountRequired;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 LevelRequired;
+};
+
+USTRUCT(BlueprintType)
+struct FBuildingRequirements
+{
+	GENERATED_USTRUCT_BODY()
+
+	FBuildingRequirements(){}
+
+	FBuildingRequirements(const TMap<EBuildingType, FBuildingRequirement> Required)
+	{
+		this->Required = Required;
+	}
+	
+	FBuildingRequirements(const EBuildingType BuildingType, const FBuildingRequirement BuildingRequirement)
+	{
+		AddRequirement(BuildingType, BuildingRequirement);
+	}
+
+	void AddRequirement(const EBuildingType BuildingType, const FBuildingRequirement BuildingRequirement)
+	{
+		Required.Add(BuildingType, BuildingRequirement);
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<EBuildingType, FBuildingRequirement> Required = TMap<EBuildingType, FBuildingRequirement>();
+};
